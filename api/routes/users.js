643f233d062e75418a9de738045a75fd73cf8ca4
Ali Kemal Çelik <alikemalcelik42@gmail.com>
2026-07-03 11:12:35 +0300

    .

--- api/routes/users.js
+++ api/routes/users.js
@@ -264,28 +264,38 @@ router.post('/firstadd', async function(req, res) {
 });
 
 router.post("/auth", async function (req, res) {
-    let { email, password } = req.body;
-    Users.validateFieldsBeforeAuth(email, password);
+    try {
+        let { email, password } = req.body;
+        Users.validateFieldsBeforeAuth(email, password);
 
-    let user = await Users.findOne({email: email});
-    if(!user) throw new CustomError(Enum.HTTP_CODES.UNAUTHORIZED, "Validation Error", "Email or password wrong");
+        let user = await Users.findOne({email: email});
+        if(!user) throw new CustomError(Enum.HTTP_CODES.UNAUTHORIZED, "Validation Error", "Email or password wrong");
 
-    if(!user.validatePassword(password)) throw new CustomError(Enum.HTTP_CODES.UNAUTHORIZED, "Validation Error", "Password wrong");
+        console.log(user.validatePassword(password));
+        console.log(user.password);
+        console.log(password);
+        await console.log(bcrypt.compareSync(user.password, password));
+        if(!user.validatePassword(password)) throw new CustomError(Enum.HTTP_CODES.UNAUTHORIZED, "Validation Error", "Password wrong");
 
-    let payload = {
-        id: user._id,
-        exp: parseInt(Date.now() / 1000) + config.JWT.EXPIRE_TIME
+
+        let payload = {
+            id: user._id,
+            exp: parseInt(Date.now() / 1000) + config.JWT.EXPIRE_TIME
+        }
+
+        let token = jwt.encode(payload, config.JWT.SECRET);
+
+        let userData = {
+            _id: user._id,
+            first_name: user.first_name,
+            last_name: user.last_name
+        }
+
+        res.json(Response.successResponse({token, user: userData}));
+    } catch(error) {
+        let errorResponse = Response.errorResponse(error);
+        res.status(errorResponse.code).json(errorResponse); 
     }
-
-    let token = jwt.encode(payload, config.JWT.SECRET);
-
-    let userData = {
-        _id: user._id,
-        first_name: user.first_name,
-        last_name: user.last_name
-    }
-
-    res.json(Response.successResponse({token, user: userData}));
 
 });
 
