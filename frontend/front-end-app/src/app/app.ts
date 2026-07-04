fee632f2bddda6157ba0f6b583ae85e66f63db1c
Ali Kemal Çelik <alikemalcelik42@gmail.com>
2026-07-04 13:35:50 +0300

    .

--- frontend/front-end-app/src/app/app.ts
+++ frontend/front-end-app/src/app/app.ts
@@ -1,15 +1,14 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-
+import { Navi } from './components/navi/navi';
+import { Category } from './components/category/category';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule, CommonModule],
+  imports: [RouterOutlet, FormsModule, CommonModule, Navi, Category],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  
-}
+export class App {}
